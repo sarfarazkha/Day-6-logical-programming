2cefebb11b861b8f70b6554fdf1188a2df7e5d45
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-06-05 23:34:31 +0530

    To Binary problem

---
+++ src/com/bridgelabz/Junit/ToBinary.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.Junit;
+
+import java.util.Scanner;
+
+public class ToBinary {
+    public static void printBinary(int num) {
+        for (int i = 31; i >= 0; i--) {
+            System.out.print(" " + (num >> i & 1));
+        }
+        System.out.printf("\n");
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        printBinary(num);
+        scanner.close();
+    }
+
+}
